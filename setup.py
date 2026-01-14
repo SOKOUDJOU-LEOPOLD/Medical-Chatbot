36c46ee6182a421209d5333ef06e3405f2c7ec8c
SOKOUDJOU Leopold <leopoldgatsing@gmail.com>
2026-01-13 20:21:41 -0500

    setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setup import find_packages, setup
+setup(
+    name = "Medical-Chatbot",
+    version = "0.1.0",
+    author = "Leopold Sokoudjou",
+    author_email = "leopoldgatsing@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+
+)
