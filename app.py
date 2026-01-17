f7ee0c1756ae0710ab6ce187797f0c7b77ec27a8
SOKOUDJOU Leopold <leopoldgatsing@gmail.com>
2026-01-16 22:09:54 -0600

    updated the app.py to connect prompt, llm and vector db(pinecone)

--- app.py
+++ app.py
@@ -11,10 +11,49 @@ import os
 
 app = Flask(__name__)
 
+load_dotenv()
+
+PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
+OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
+
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+embeddings = download_hugging_face_embeddings()
+
+index_name = "medical-chatbot" 
+# Embed each chunk and upsert the embeddings into your Pinecone index.
+docsearch = PineconeVectorStore.from_existing_index(
+    index_name=index_name,
+    embedding=embeddings
+)
+
+retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
+
+chatModel = ChatOpenAI(model="gpt-4o")
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
+
+question_answer_chain = create_stuff_documents_chain(chatModel, prompt)
+rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+
 @app.route("/")
 def index():
     return render_template('chat.html')
 
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    response = rag_chain.invoke({"input":input})
+    print("Response: ", response["answer"])
+    return str(response["answer"])
+
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 8080, debug = True)
-    
